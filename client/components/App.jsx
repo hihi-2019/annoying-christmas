282746f91671f930a4e794a7d85cd747b4cfa674
Alice Alsford <alice.alsford@gmail.com>
2019-11-08 10:40:18 +1300

    set up presskey

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
 import ChristmasTree from './ChristmasTree'
+import { HashRouter as Router, Route } from 'react-router-dom'
 
 import Navbar from './Navbar'
-
+import Carols from './Carols'
 
 const App = () => {
   return (
-    <div> 
-    <Navbar/>
-     <ChristmasTree />
-     </div>
+    <div>
+      <Router>
+        <React.Fragment>
+          <Navbar />
+          <ChristmasTree />
+          <Route exact path="/carols" component={Carols} />
+        </React.Fragment>
+      </Router>
+
+    </div>
   )
 }
 
