535175e13aa382577e8efca876b0de7ae912b274
Student <student@eda-bc-05.localdomain>
2019-11-08 10:28:39 +1300

    created lights on click

--- client/components/ChristmasTree.jsx
+++ client/components/ChristmasTree.jsx
@@ -4,7 +4,6 @@ import Star from './Star'
 
 const ChristmasTree = () => {
     return(
-        
       <div className = 'background'>
           <Star width={window.innerWidth} height={window.innerHeight}/>
       </div>
