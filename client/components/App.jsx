a0cf3df7ccb2f05c9d6fbfadf6bce6333c194b1b
Student <student@eda-bc-01.localdomain>
2019-11-07 18:25:13 +1300

    navbar component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './Navbar'
+
 
 const App = () => {
   return (
-    <h1>React development has begun!</h1>
+    <Navbar/>
   )
 }
 
